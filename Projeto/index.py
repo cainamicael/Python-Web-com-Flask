5342587c674ec2bdc40b818e05dfb0a91ec02c6b
cainamicael <cainamicaeloficial@gmail.com>
2023-06-29 19:06:01 -0300

    add

---
+++ Projeto/index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Rotas
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/sobre')
+def sobre():
+    return '<h2>Sobre</h2>'
+
+#pegando parâmetro da url
+@app.route('/noticia/<slug>')
+def noticia(slug):
+    return f'<h2>O slug é {slug}</h2>'
+
