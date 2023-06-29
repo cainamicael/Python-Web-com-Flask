cc4d53951844c823efc107808886d94c20015d96
cainamicael <cainamicaeloficial@gmail.com>
2023-06-29 20:02:10 -0300

    add

--- Projeto/index.py
+++ Projeto/index.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 
 app = Flask(__name__)
+app.secret_key = 'vad6gq883JYTI!&uhje31ljhj,cw$%¨defkqrq8'
 
 #Rotas
 @app.route('/', methods=['GET', 'POST'])
@@ -24,3 +25,26 @@ def sobre():
 @app.route('/noticia/<slug>')
 def noticia(slug):
     return f'<h2>O slug é {slug}</h2>'
+
+@app.route('/sessao')
+def sessao():
+    if 'usuario' in session:
+        usuario = session['usuario']
+        return usuario
+    else:
+        session['usuario'] = 'guilherme' 
+        return session['usuario'] 
+    
+@app.route('/cookie')
+def cookie():
+    #Pegando cookie
+    if (request.cookies.get('usuario')):
+        resp = make_response('Meu website com o cookie setado')
+    else:
+        #Setando cookie
+        resp = make_response('Meu website sem o cookie')
+        resp.set_cookie('usuario', 'guilherme')    
+    
+    return resp
+    
+
