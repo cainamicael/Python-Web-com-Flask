5f53f00b8ea8a103c2bab8e468f029f222f5dc3a
cainamicael <cainamicaeloficial@gmail.com>
2023-06-29 19:11:55 -0300

    add

--- Projeto/index.py
+++ Projeto/index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 #Rotas
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', content=['banana', 'pera', 'maçã'])
 
 @app.route('/sobre')
 def sobre():
