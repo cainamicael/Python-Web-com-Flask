de4087d8459f5d9ebc16ed658267e253910c8917
cainamicael <cainamicaeloficial@gmail.com>
2023-06-30 15:50:23 -0300

    add

---
+++ Projeto/estudo.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, session, make_response
+import pymysql
+
+app = Flask(__name__)
+app.secret_key = 'vad6gq883JYTI!&uhje31ljhj,cw$%¨defkqrq8'
+
+db = pymysql.connect(host='localhost', user='root', password='', database='youtube')
+
+#Rotas
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('estudo.html')
+    else:
+        nome = request.form['nome']
+        idade = request.form['idade']
+        conteudo = request.form['conteudo']
+        
+        col = {'nome': nome, 'idade': idade, 'conteudo': conteudo} #collection
+        
+        return f"O que veio do meu form: {col}" #o name do formulario
+
+@app.route('/sobre')
+def sobre():
+    return '<h2>Sobre</h2>'
+
+#pegando parâmetro da url
+@app.route('/noticia/<slug>')
+def noticia(slug):
+    return f'<h2>O slug é {slug}</h2>'
+
+@app.route('/sessao')
+def sessao():
+    if 'usuario' in session:
+        usuario = session['usuario']
+        return usuario
+    else:
+        session['usuario'] = 'guilherme' 
+        return session['usuario'] 
+    
+@app.route('/cookie')
+def cookie():
+    #Pegando cookie
+    if (request.cookies.get('usuario')):
+        resp = make_response('Meu website com o cookie setado') #Posso usar um render_template
+    else:
+        #Setando cookie
+        resp = make_response('Meu website sem o cookie') #Posso usar um render_template
+        resp.set_cookie('usuario', 'guilherme')    
+    
+    return resp
+  
+@app.route('/mysql')
+def mysql():
+    cursor = db.cursor()
+    sql = 'SELECT * FROM clientes'
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    
+    array = []
+    
+    for elemento in results:
+        array.append(elemento)
+        
+    print(results)
+    return array
+
+app.run(port=5000, host='localhost', degub=True)
--- Projeto/index.py
+++ Projeto/index.py
@@ -1,68 +1,34 @@
-from flask import Flask, render_template, request, session, make_response
+from flask import Flask, request, render_template, redirect, url_for
 import pymysql
 
 app = Flask(__name__)
-app.secret_key = 'vad6gq883JYTI!&uhje31ljhj,cw$%¨defkqrq8'
-
 db = pymysql.connect(host='localhost', user='root', password='', database='youtube')
 
-#Rotas
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
-    if request.method == 'GET':
-        return render_template('index.html')
-    else:
-        nome = request.form['nome']
-        idade = request.form['idade']
-        conteudo = request.form['conteudo']
-        
-        col = {'nome': nome, 'idade': idade, 'conteudo': conteudo} #collection
-        
-        return f"O que veio do meu form: {col}" #o name do formulario
-
-@app.route('/sobre')
-def sobre():
-    return '<h2>Sobre</h2>'
-
-#pegando parâmetro da url
-@app.route('/noticia/<slug>')
-def noticia(slug):
-    return f'<h2>O slug é {slug}</h2>'
-
-@app.route('/sessao')
-def sessao():
-    if 'usuario' in session:
-        usuario = session['usuario']
-        return usuario
-    else:
-        session['usuario'] = 'guilherme' 
-        return session['usuario'] 
-    
-@app.route('/cookie')
-def cookie():
-    #Pegando cookie
-    if (request.cookies.get('usuario')):
-        resp = make_response('Meu website com o cookie setado') #Posso usar um render_template
-    else:
-        #Setando cookie
-        resp = make_response('Meu website sem o cookie') #Posso usar um render_template
-        resp.set_cookie('usuario', 'guilherme')    
-    
-    return resp
-  
-@app.route('/mysql')
-def mysql():
+@app.route('/clientes', methods=['GET'])
+def main():
     cursor = db.cursor()
     sql = 'SELECT * FROM clientes'
     cursor.execute(sql)
     results = cursor.fetchall()
-    
-    array = []
-    
-    for elemento in results:
-        array.append(elemento)
-        
     print(results)
-    return array
+    
+    return render_template('index.html', content=results)
 
-app.run(port=5000, host=localhost, degub=True)
+@app.route('/clientes', methods=['POST'])
+def cadastrar():
+    nome = request.form['nome']
+    email = request.form['email']
+    
+    cursor = db.cursor()
+    sql = 'INSERT INTO clientes (nome, email) VALUES (%(nome)s, %(email)s)'
+    res = cursor.execute(sql, {'nome': nome, 'email': email})
+    db.commit()
+    
+    return redirect(url_for('main')) #O nome da função do endpoint que queremos
+
+@app.route('/deletar') #/deletar?id={{elemento[0]}}
+def delete():
+    id = request.args.get('id')
+    return f'O id é {id}'
+
+app.run(port=5000, host='localhost', debug=True)
